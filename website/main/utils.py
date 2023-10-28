80902755c0767798b4fc980f9488b40799810dc3
unknown <fahadbinu90@outlook.com>
2023-10-28 12:41:20 +0400

    remove website

--- website/main/utils.py
+++
@@ -1,9 +0,0 @@
-from .models import *
-
-def cartData(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, cart_complete=False)
-        order_products = Order_Product.objects.filter(order=order)
-        
-    return {'order_products':order_products, 'order':order}
